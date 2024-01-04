a8effed5646861ef446bedc9b12cf77bbef05b47
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-04 13:10:13 +0400

    change background component places

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <MainBackground />
         <Header />
         {children}
       </body>
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,30 +4,33 @@ import Link from "next/link";
 import Image from "next/image";
 // Photo source import
 import LogoImage from "@/assets/logo.png";
+import MainBackground from "../mainBackground/MainBackground";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href={"/"} className={styles.logo}>
-        <Image
-          src={LogoImage}
-          className={styles.logoImg}
-          alt="a plate with food"
-          priority
-        />
-        NextLevel Food
-      </Link>
-
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href={"/meals"}>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href={"/community"}>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainBackground />
+      <header className={styles.header}>
+        <Link href={"/"} className={styles.logo}>
+          <Image
+            src={LogoImage}
+            className={styles.logoImg}
+            alt="a plate with food"
+            priority
+          />
+          NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
