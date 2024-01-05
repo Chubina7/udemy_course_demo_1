ab7e7b8d75d2b08e6df89cb22a089e9694f2d57b
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-05 16:20:04 +0400

    fetched one dynamic data and made appropriate ui

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/MealsGrid";
-import getMeals from "@/lib/meals";
+import { getMeals } from "@/lib/meals";
 
 async function Meals() {
   const data = await getMeals();
