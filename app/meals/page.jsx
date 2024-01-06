2aa607eb8b8ee619d8d9c0c4575cfe07c569d66a
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-06 11:31:39 +0400

    add metadata

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -4,6 +4,11 @@ import Link from "next/link";
 import MealsGrid from "@/components/meals/MealsGrid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All meals",
+  description: "Browse delicious meals shared by our vibrant community.",
+};
+
 async function Meals() {
   const data = await getMeals();
 
